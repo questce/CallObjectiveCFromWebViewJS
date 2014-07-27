3926630dad02b7eaa47b45ce7ea7feeda2da9436
questce <ddsquestcequisepasse@gmail.com>
2014-07-28 00:10:41 +0900

    Finsih Call ObjeciveC from WebView JavaScript

--- CallObjectiveCFromWebViewJS/ViewController.m
+++ CallObjectiveCFromWebViewJS/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
 
 @end
 
@@ -17,13 +17,47 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSString* htmlFilePath = [[NSBundle mainBundle] pathForResource:@"res/sample" ofType:@"html"];
+    NSString* htmlString = [[NSString alloc] initWithContentsOfFile:htmlFilePath encoding:NSUTF8StringEncoding error:nil];
+    NSURL *resURL = [[NSBundle mainBundle] URLForResource:@"res/sample" withExtension:@"html"];
+    self.sampleUIWebView.delegate = self;
+    [self.sampleUIWebView loadHTMLString:htmlString baseURL:resURL];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if ([request.URL.scheme isEqualToString:@"js"]) {
+        // custom scheme
+        [self callNativeMethod: request];
+        return NO;
+    } else {
+        // except custom scheme start load request
+        return YES;
+    }
+}
+
+- (void)callNativeMethod:(NSURLRequest*)request
+{
+    if([request.URL.host isEqualToString:@"Hello"]){
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Native Method"
+                                                        message:@"Native Method called from javascript"
+                                                       delegate:nil
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"OK", nil];
+        [alert show];
+    }
 }
 
 @end
